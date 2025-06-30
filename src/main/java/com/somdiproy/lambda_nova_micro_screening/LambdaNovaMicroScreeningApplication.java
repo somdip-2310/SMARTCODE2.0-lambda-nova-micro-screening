5de06b7f86791f693fd5e76d7a15fc5e222908ca
Somdip Roy <somdiproy.roy@gmail.com>
2025-06-30 22:00:29 +0530

    Initial commit

---
+++ src/main/java/com/somdiproy/lambda_nova_micro_screening/LambdaNovaMicroScreeningApplication.java
@@ -0,0 +1,13 @@
+package com.somdiproy.lambda_nova_micro_screening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdaNovaMicroScreeningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambdaNovaMicroScreeningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/somdiproy/lambda_nova_micro_screening/LambdaNovaMicroScreeningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somdiproy.lambda_nova_micro_screening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambdaNovaMicroScreeningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
